b9d4c9c2afa408b54442df3787c2437b053b7675
Joseph Campuzano <campuzanojoseph@gmail.com>
2026-09-19 14:50:38 -0500

    Update footer copyright year automatically

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer class="border-t bg-accent p-4 text-center text-xs text-muted-foreground">
-      © 2025 ExpenseMate. All rights reserved.
+      © {new Date().getFullYear()} ExpenseMate. All rights reserved.
     </footer>
   );
 }
